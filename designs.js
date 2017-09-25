1a1318af29cc860dccbdaf38db5195b095b355d7
nraovic <n.raovic@gmail.com>
2017-09-25 22:45:20 +0200

    feat: create paintCell function

--- designs.js
+++ designs.js
@@ -1,21 +1,29 @@
 //add event listener to the submit button to create the canvas(grid) with the makeGrid funtion
 document.getElementById("sizePicker").addEventListener("submit", makeGrid);
 
+//change the background color of a cell 
+function paintCell(evt) {
+    //get the color from the color picker
+    const color = document.getElementById('colorPicker').value;
+    //set it as a background color of the cell
+    evt.currentTarget.style.backgroundColor = color;
+}
+
 function makeGrid(evt) {
     //clear the previous canvas (table)
     const canvas = document.getElementById("pixel_canvas");
     while (canvas.firstChild) {
         canvas.removeChild(canvas.firstChild);
     }
-    
+
     //get the values of canvas' width and height
     const width = document.getElementById('input_width').value;
     const height = document.getElementById('input_height').value;
-    
+
     //create canvas
     //loop through the height value to append rows to the canvas
     let i = 0;
-    while (i < height) {   
+    while (i < height) {
         const row = document.createElement("tr");
         canvas.appendChild(row);
         //loop through the width value to append cells to the canvas
@@ -24,12 +32,7 @@ function makeGrid(evt) {
             const cell = document.createElement("td");
             row.appendChild(cell);
             //add event listener to each cell to change the background color on 'click'
-            cell.addEventListener("click", function () {
-                //get the color from the color picker
-                const color = document.getElementById('colorPicker').value;
-                //set it as a background color of the cell
-                cell.style.backgroundColor = color;
-            });
+            cell.addEventListener("click", paintCell);
             j++;
         }
         i++;
